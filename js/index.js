50f9db785f000e911039349aa44f1d0aa2052f67
蒲永佳 <1737426012@qq.com>
2022-05-22 10:07:07 +0800

    '轮播图完成'

--- js/index.js
+++ js/index.js
@@ -1,7 +1,10 @@
 var slideshow_1 = document.getElementsByClassName('slideshow_1')[0];
 var num = 0;
+var timer;
+var time;
 function fu() {
-    var time = setInterval(function () {
+    clearInterval(time);
+    time = setInterval(function () {
         num += -10;
         slideshow_1.firstElementChild.style.marginLeft = num + 'px'
         if (num == -1000) {
@@ -9,10 +12,35 @@ function fu() {
             slideshow_1.appendChild(slideshow_1.firstElementChild);
             num = 0;
             clearInterval(time);
-            setTimeout(function () {
+            timer = setTimeout(function () {
                 fu()
-            }, 1000)
+            }, 3000)
         }
-    }, 18)
+    }, 15)
 }
 fu()
+
+var rig = document.getElementsByClassName('rig')[0]
+var let = document.getElementsByClassName('let')[0];
+rig.onclick = function () {
+    clearTimeout(timer);
+    fu();
+}
+let.onclick = function () {
+    clearTimeout(timer);
+    clearTimeout(time);
+
+    var timet = setInterval(function () {
+        num += 10;
+        slideshow_1.firstElementChild.style.marginLeft = num + 'px'
+        if (num == 1000) {
+            slideshow_1.firstElementChild.style.marginLeft = 0 + 'px'
+            slideshow_1.insertBefore(slideshow_1.lastElementChild, slideshow_1.firstElementChild);
+            num = 0;
+            clearInterval(timet);
+            timer = setTimeout(function () {
+                fu()
+            }, 3000)
+        }
+    }, 15)
+}
