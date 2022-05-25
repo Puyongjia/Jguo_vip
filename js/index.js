0c411f166180486e2cb4835d1203fcee1f453279
蒲永佳 <1737426012@qq.com>
2022-05-25 08:31:39 +0800

    '修护bug'

--- js/index.js
+++ js/index.js
@@ -1,18 +1,18 @@
 var slideshow_1 = document.getElementsByClassName('slideshow_1')[0];
 var num = 0;
-var timer;
-var time;
+var timer2;
+var time11;
 function fu() {
-    clearInterval(time);
-    time = setInterval(function () {
+    clearInterval(time11);
+    time11 = setInterval(function () {
         num += -10;
         slideshow_1.firstElementChild.style.marginLeft = num + 'px'
         if (num == -1000) {
             slideshow_1.firstElementChild.style.marginLeft = 0 + 'px'
             slideshow_1.appendChild(slideshow_1.firstElementChild);
             num = 0;
-            clearInterval(time);
-            timer = setTimeout(function () {
+            clearInterval(time11);
+            timer2 = setTimeout(function () {
                 fu()
             }, 2000)
         }
@@ -23,12 +23,12 @@ fu()
 var rig = document.getElementsByClassName('rig')[0]
 var let = document.getElementsByClassName('let')[0];
 rig.onclick = function () {
-    clearTimeout(timer);
+    clearTimeout(timer2);
     fu();
 }
 let.onclick = function () {
-    clearTimeout(timer);
-    clearTimeout(time);
+    clearTimeout(timer2);
+    clearTimeout(time11);
 
     var timet = setInterval(function () {
         num += 10;
@@ -38,7 +38,7 @@ let.onclick = function () {
             slideshow_1.insertBefore(slideshow_1.lastElementChild, slideshow_1.firstElementChild);
             num = 0;
             clearInterval(timet);
-            timer = setTimeout(function () {
+            timer2 = setTimeout(function () {
                 fu()
             }, 3000)
         }
